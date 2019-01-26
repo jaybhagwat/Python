a517d69fb6d47162a0097fa4c9d7f12f96e6e4a3
Jay Manoj Bhagwat <jay7bhagwat@gmail.com>
2019-01-26 19:26:18 +0530

    Update String_operations.py

--- Homeworks/String_operations.py
+++ Homeworks/String_operations.py
@@ -6,8 +6,16 @@ print('T' in s[0])
 #Write a program to accept two strings from user and check if second string is rotation of first  Input:1.manager 2.germana Output=True
 j=str(input("Enter first String"))
 k=str(input("Enter second String"))
-print(j in k)
+if(len(j)==len(k)):
+	temp=j+j
+	if(k in temp):
+		print("String is rotational")
+	else:
+		print("String is not rotational")
 
 #write a program to accept a string from user and convert it to lowercase, upppercase.
 z=str(input("Enter the string"))
-print(lower(z,1))
+if(z.islower()):
+	print(z.upper())
+else:
+	print(z.lower())
