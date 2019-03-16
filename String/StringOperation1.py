0993e97fc89c2eaedeed70fde3229e19e054cae0
Jay Manoj Bhagwat <jay7bhagwat@gmail.com>
2019-03-16 19:40:52 +0530

    Add files via upload

---
+++ String/StringOperation1.py
@@ -0,0 +1,27 @@
+my_string='Hello'
+print(my_string)
+
+my_string="hello"
+print(my_string)
+
+my_string='''Hello'''
+print(my_string)
+
+my_string="""This is multiline comment
+so we can write on another line"""
+print(my_string)
+
+str='jaybhagwat'
+print('str=',str)
+
+#for first character
+print('str[0]=',str[0])
+
+#for last character
+print('str[-1]=',str[-1])
+
+#slicing 2nd and 5th character
+print('str[1:5]=',str[1:5])
+
+#slicing string from 6th to 2nd last character
+print('str[5:-2]=',str[5:-2])
